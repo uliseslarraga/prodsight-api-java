630330b37828adab251c1270886dbfa9d4a62bdc
Ulises Larraga <uliseslarraga@gmail.com>
2026-02-17 11:59:12 -0600

    Initial commit

---
+++ src/main/java/com/prodsight/app/Prodsight/ProdsightApplication.java
@@ -0,0 +1,13 @@
+package com.prodsight.app.Prodsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdsightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prodsight/app/Prodsight/ProdsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prodsight.app.Prodsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
