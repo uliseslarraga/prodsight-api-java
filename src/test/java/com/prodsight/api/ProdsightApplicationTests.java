f290cfbd093d573ebaf0c74506e578920c977625
Ulises Larraga <uliseslarraga@gmail.com>
2026-02-18 10:00:35 -0600

    feat: update tests

--- src/test/java/com/prodsight/api/ProdsightApplicationTests.java
+++ src/test/java/com/prodsight/api/ProdsightApplicationTests.java
@@ -1,15 +1,8 @@
 package com.prodsight.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = com.prodsight.api.ProdsightApplication.class,
-properties = {
-  "spring.autoconfigure.exclude=" +
-    "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-    "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
-}
-)
+@SpringBootTest
 class ProdsightApplicationTests {
 
 	void contextLoads() {
