9c5501dfb4918daab4bd26a3042b6d487a1efe57
Ulises Larraga <uliseslarraga@gmail.com>
2026-02-19 17:17:46 -0600

    feat: update sqs bean

--- src/main/java/com/prodsight/api/config/SqsConfig.java
+++ src/main/java/com/prodsight/api/config/SqsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
@@ -19,13 +20,20 @@ public class SqsConfig {
       @Value("${app.sqs.endpoint:}") String endpoint
   ) {
     var builder = SqsClient.builder()
-        .region(Region.of(region))
-        .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test")));
+        .region(Region.of(region));
 
-    // For LocalStack: set endpoint (e.g. http://localhost:4566)
+    // LocalStack only: endpoint override + dummy creds
     if (endpoint != null && !endpoint.isBlank()) {
-      builder = builder.endpointOverride(URI.create(endpoint));
+      builder = builder
+          .endpointOverride(URI.create(endpoint))
+          .credentialsProvider(
+              StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test"))
+          );
+    } else {
+      // AWS (ECS): use task role creds
+      builder = builder.credentialsProvider(DefaultCredentialsProvider.create());
     }
+
     return builder.build();
   }
 }
